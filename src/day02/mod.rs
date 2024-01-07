e29469520b8138d91d926e820167c5c20511adb9
Jon Gunnar Fossum <jon.gunnar.fossum@gmail.com>
2024-01-07 16:33:42 +0100

    Add skeleton for day 2

---
+++ src/day02/mod.rs
@@ -0,0 +1,11 @@
+
+// cube set: 12 red cubes, 13 green cubes, and 14 blue cubes
+// determine legal/possible games
+// sum the ID numbers 
+
+
+
+
+pub fn day02() {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod day01;
+mod day02;
 mod day06;
 
 // TODO: add a decition tree, CLI interface, or something such that everything isn't just run in sequentional order.
 fn main() {
     day01::day01();
+    day02::day02();
     day06::day06();
 }
