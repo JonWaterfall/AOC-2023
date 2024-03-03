95b5e05a90f3b88b7d3fccc161181175ffe2285b
Jon Gunnar Fossum <jon.gunnar.fossum@gmail.com>
2024-03-03 14:47:31 +0100

    add basic flow control
    No need to run everything.
    Next up will be to accept some user input instead of hard coded value change.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,16 @@ mod day06;
 
 // TODO: add a decition tree, CLI interface, or something such that everything isn't just run in sequentional order.
 fn main() {
-    day01::day01();
-    day02::day02();
-    day06::day06();
+    let mut daychoice: i32 = 0;
+
+    daychoice = 2;
+    match daychoice {
+
+        1 => day01::day01(),
+        2 => day02::day02(),
+        
+        6 => day06::day06(),
+
+        _ => println!("No valid choice given"),
+    }
 }
